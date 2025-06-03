ea17617d9c6dc5f2a15e66867a3adb42a3d830cc
Daniel <bdani9308@gmail.com>
2025-06-03 21:04:31 +0200

    add solutions up to 05_ticket_v2

--- exercises/05_ticket_v2/15_outro/src/description.rs
+++ exercises/05_ticket_v2/15_outro/src/description.rs
@@ -1,8 +1,43 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `TicketDescription` type,
 //   enforcing that the description is not empty and is not longer than 500 bytes.
 //   Implement the traits required to make the tests pass too.
-
+#[derive(Debug,PartialEq,Clone)]
 pub struct TicketDescription(String);
+
+#[derive(Debug,thiserror::Error)]
+pub enum TicketDescriptionError  {
+     #[error("The description cannot be empty")]
+    DescriptionEmpty,
+     #[error("The description cannot be longer than 500 bytes")]
+    DescriptionTooLong
+}
+
+pub fn validate_description(value: &str) -> Result<(),TicketDescriptionError>{
+    if value.is_empty(){
+        return Err(TicketDescriptionError::DescriptionEmpty);
+    }
+    if value.len()>500{
+        return Err(TicketDescriptionError::DescriptionTooLong);
+    }
+    Ok(())
+}
+
+impl TryFrom<String> for TicketDescription {
+    type Error = TicketDescriptionError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for TicketDescription {
+    type Error = TicketDescriptionError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        validate_description(&value)?;
+        Ok(Self(value.to_string()))
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/05_ticket_v2/15_outro/src/title.rs
+++ exercises/05_ticket_v2/15_outro/src/title.rs
@@ -2,7 +2,44 @@
 //   enforcing that the title is not empty and is not longer than 50 bytes.
 //   Implement the traits required to make the tests pass too.
 
+#[derive(Debug,PartialEq,Clone)]
 pub struct TicketTitle(String);
+
+
+#[derive(Debug,thiserror::Error)]
+pub enum TicketError{
+    #[error("The title cannot be empty")]
+    TitleEmpty,
+    #[error("The title cannot be longer than 50 bytes")]
+    TitleTooLong
+}
+
+pub fn validate_title(value: &str) -> Result<(), TicketError>{
+        if value.is_empty() {
+            return Err(TicketError::TitleEmpty)
+        }
+        if value.len()>50 {
+            return Err(TicketError::TitleTooLong)
+        }
+        Ok(())
+}
+
+impl TryFrom<String> for TicketTitle {
+    type Error = TicketError;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.as_str().try_into()
+    }
+}
+
+impl TryFrom<&str> for TicketTitle {
+    type Error = TicketError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        validate_title(value)?;
+        Ok(TicketTitle(value.to_string()))
+    }
+}
 
 #[cfg(test)]
 mod tests {
